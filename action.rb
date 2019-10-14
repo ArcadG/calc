976db39c5757dbf7eb4568e5aca7185b011f1274
Pavel Maksimenko <pavel.g.maksimenko@gmail.com>
2019-10-14 23:45:17 +0200

    Refactoring

--- action.rb
+++ action.rb
@@ -1,20 +1,20 @@
 class Action
-    def perform(operator:, a:, b:)
-        case operator
-        when '+'
-           sum(a, b)
-        when '-'
-           minus(a, b)
-        end
+  def perform(operator:, a:, b:)
+    case operator
+    when '+'
+      sum(a, b)
+    when '-'
+      minus(a, b)
     end
+  end
 
-    private
+  private
 
-    def sum(a, b)
-        a + b
-    end
+  def sum(a, b)
+    a + b
+  end
 
-    def minus(a, b)
-        a - b
-    end
+  def minus(a, b)
+    a - b
+  end
 end
