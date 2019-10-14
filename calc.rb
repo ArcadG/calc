c62e6028a43a9ca8bfa894f3d0e4139def7e829c
Аркадий Анатольевич <battin@192.168.1.6>
2019-10-14 22:11:03 +0300

    calc V2

--- calc.rb
+++ calc.rb
@@ -7,13 +7,37 @@ class Calc
     end
 
     def read_operands
-        s = gets.chomp
-        arr = s.split(/\d\s/)
-        arr1 = s.split(/\D\s/)
+        s = gets.chomp.delete(' ')
+        arr = s.split(/\d/)
+        arr1 = s.split(/\D/)
         @a = arr1[0].to_i
         @operator = arr[-1]
-        @b = arr1[1].to_i 
+        @b = arr1[-1].to_i
+
+        if arr[0] == "-"
+            @a = arr1[1].to_i
+            @a = -@a
+        end
+
+        if @operator == "+-"
+            @operator = "+"
+            @b = -@b
+        end
+
+        if @operator == "--"
+            @operator = "-"
+            @b = -@b
+        end
     end
+    
+    #def read_operands
+        #s = gets.chomp.delete(' ')
+        #arr = s.split(/\d/)
+        #arr1 = s.split(/\D/)
+        #@a = arr1[0].to_i
+        #@operator = arr[-1]
+        #@b = arr1[1].to_i 
+    #end 
 
     #def read_operands
         #arrey = gets.split 
